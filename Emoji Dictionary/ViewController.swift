ace3c2fc49c0617ffa802aa9f0bbef005a82b78a
marc <MDESDIER@HOTMAIL.COM>
2018-11-07 22:20:52 -0800

    Initial Commit

---
+++ Emoji Dictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by tester on 11/7/18.
+//  Copyright © 2018 Tepo Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
